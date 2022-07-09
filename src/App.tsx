5eba5940e3e7db0cb98d8752629199e93dd51b26
hdu35 <hanyi.du.cs@gmail.com>
2022-07-09 20:07:38 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,70 @@
+import React, { Suspense } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { NavLink, Route, Routes } from "react-router-dom";
+import styled from "@emotion/styled";
+import ErrorBoundary from "components/ErrorBoundary";
+
+const AppWrapper = styled.div`
+  display: flex;
+  flex-flow: column;
+  align-items: flex-start;
+`;
+const HeaderWrapper = styled.div`
+  width: 100%;
+  justify-content: space-between;
+  position: fixed;
+  top: 0;
+  z-index: 2;
+`;
+
+const BodyWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  padding: 120px 16px 0px 16px;
+  align-items: center;
+  flex: 1;
+  z-index: 1;
+`;
+
+function App() {
+  return (
+    <ErrorBoundary>
+      <AppWrapper>
+        <HeaderWrapper>
+          <div>
+            <h1>hdu35</h1>
+            <nav>
+              <ul>
+                <li>
+                  <NavLink to={"/"}>Home</NavLink>
+                </li>
+                <li>
+                  <NavLink to={"about"}>About</NavLink>
+                </li>
+                <li>
+                  <NavLink to={"blogs"}>Blogs</NavLink>
+                </li>
+                <li>
+                  <NavLink to={"gallery"}>Gallery</NavLink>
+                </li>
+                <li>
+                  <NavLink to={"contact"}>Contact</NavLink>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </HeaderWrapper>
+      </AppWrapper>
+
+      <BodyWrapper>
+        <Suspense fallback={null}>
+          <Routes></Routes>
+        </Suspense>
+      </BodyWrapper>
+    </ErrorBoundary>
+  );
+}
+
+export default App;
---
+++ src/components/ErrorBoundary/index.tsx
@@ -0,0 +1,34 @@
+import React, { ErrorInfo } from "react";
+
+type ErrorBoundaryState = {
+  error: Error | null;
+  hasError: boolean;
+};
+
+export default class ErrorBoundary extends React.Component<any, ErrorBoundaryState> {
+  constructor(props: any) {
+    super(props);
+    this.state = { hasError: false, error: null };
+  }
+
+  static getDerivedStateFromError(error: Error) {
+    // Update state so the next render will show the fallback UI.
+    console.log(error);
+    return { hasError: true };
+  }
+
+  componentDidCatch(error: Error, errorInfo: ErrorInfo) {
+    // You can also log the error to an error reporting service
+    console.log(error);
+    console.log(errorInfo);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      // You can render any custom fallback UI
+      return <h1>Something went wrong.</h1>;
+    }
+
+    return this.props.children;
+  }
+}
